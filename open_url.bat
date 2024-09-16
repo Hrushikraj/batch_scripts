3a65ed5a6d227832ed9426f0278d636b2690954b
HRUSHIK RAJ S <88938793+Hrushikraj@users.noreply.github.com>
2024-09-16 22:29:06 +0530

    Add files via upload

---
+++ open_url.bat
@@ -0,0 +1,5 @@
+@echo off
+Title Portfolio-prompt
+echo Hello myself hrushik click enter to see my portfolio
+pause
+start chrome.exe https://hrushikraj.github.io/portfolio/
---
+++ open_url_incognito.bat
@@ -0,0 +1,5 @@
+@echo off
+Title Portfolio-prompt
+echo Hello myself hrushik click enter to see my portfolio
+pause
+start chrome.exe -incognito https://hrushikraj.github.io/portfolio/
